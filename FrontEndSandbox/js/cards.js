675b558a1d31d9fb1aa439f328ea3b586977eb94
William Lee <masteratsoccer@gmail.com>
2017-01-25 01:22:20 -0500

    update for Jason

--- FrontEndSandbox/js/cards.js
+++ FrontEndSandbox/js/cards.js
@@ -2,9 +2,23 @@ angular.module('MyApp')
 .controller('AppCtrl', function($scope) {
 	$scope.users = ['Fabio', 'Leonardo', 'Thomas', 'Gabriele', 'Fabrizio', 'John', 'Luis', 'Kate', 'Max'];
 
+	//initialize eventsArray[]. Populate with array.push() from firebase
+	$scope.eventsArray = [];
+
+	//add some sample events
+	$scope.eventsArray.push({
+		name: "PennApps",
+		start_time: "is this a string",
+		end_time: "end date",
+		location: "UPenn Engineering",
+		lat: 39.9522,
+		lng: -75.1932,
+		description: "A hackathon at the University of Pennsylvania"
+	});
+
+
     //view event function
     $scope.viewEvent = function() {
-    	alert("yea boi");
     	var swarthmore = {lat: 39.9021, lng: -75.3499}
     	updateMapLocation(swarthmore);
     }
@@ -37,5 +51,40 @@ function updateMapLocation(newLocation) {
 }
 
 //Google map zoom done
+
 //integrate backend with json format
 //slide out panel for event
+
+
+/*
+TODO
+	Right now, can post eventArray[0] on all cards. Need ng-repeat to work so that it's not the same event for all cards, and 
+	need to be able to access specific elements (e.g. event.name)
+
+	Maybe make another nesting div? 
+*/
+
+/*
+DONE
+	Cards
+	Map Integration
+	Button backend + map zoom
+
+*/
+
+
+/*
+JSON FORMAT
+
+ array.push({
+	var array = [];
+          name: data.name,
+          start_time: data.start_time,
+          end_time: data.end_time,
+          location: data.location,
+          lat: data.lat,
+          lng: data.lng,
+          description: data.description
+        });
+
+*/
