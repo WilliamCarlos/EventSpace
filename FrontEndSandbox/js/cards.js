204f793cc3f9baeffe2246a7be132772ad69b2a2
William Lee <masteratsoccer@gmail.com>
2017-01-26 23:14:37 -0500

    1 json example working

--- FrontEndSandbox/js/cards.js
+++ FrontEndSandbox/js/cards.js
@@ -25,6 +25,24 @@ angular.module('MyApp')
 		"description": "A hackathon at the University of Pennsylvania"
 	});
 
+	//events for hackathons
+	
+	/*$scope.yHack = JSON.parse('{
+		"name": "PennApps",
+		"start_time": "is this a string",
+		"end_time": "end date",
+		"location": "UPenn Engineering",
+		"lat": 39.9522,
+		"lng": -75.1932,
+		"description": "A hackathon at the University of Pennsylvania"}');*/
+
+		//"name":"PennApps", "start_time":"is this a string", "end_time":"end date", "location":"UPenn Engineering", "lat":39.9522, "lng":-75.1932, "description":"A hackathon at the University of Pennsylvania" 
+
+		//$scope.yHack = JSON.parse('{ "name":"John", "age":30, "city":"New York"}');
+		$scope.yHack = JSON.parse('{ "name":"PennApps", "start_time":"is this a string", "end_time":"end date", "location":"UPenn Engineering", "lat":39.9522, "lng":-75.1932, "description":"A hackathon at the University of Pennsylvania" }');
+
+
+
 	//loop through and add json elements to array
 
 	//gonna try to store json elements in a different array
@@ -39,31 +57,31 @@ angular.module('MyApp')
     }
 });
 
-function initMap() {
-	var uluru = {lat: 37.4419, lng: -122.1430};
+		function initMap() {
+			var uluru = {lat: 37.4419, lng: -122.1430};
 
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 15,
-		center: uluru
-	});
+			var map = new google.maps.Map(document.getElementById('map'), {
+				zoom: 15,
+				center: uluru
+			});
 
-	var marker = new google.maps.Marker({
-		position: uluru,
-		map: map
-	});
-}
+			var marker = new google.maps.Marker({
+				position: uluru,
+				map: map
+			});
+		}
 
-function updateMapLocation(newLocation) {
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 15,
-		center: newLocation
-	});
+		function updateMapLocation(newLocation) {
+			var map = new google.maps.Map(document.getElementById('map'), {
+				zoom: 15,
+				center: newLocation
+			});
 
-	var marker = new google.maps.Marker({
-		position: newLocation,
-		map: map
-	});
-}
+			var marker = new google.maps.Marker({
+				position: newLocation,
+				map: map
+			});
+		}
 
 //Google map zoom done
 
@@ -81,7 +99,7 @@ TODO
 	Get Swat headers
 
 	Add page
-*/
+	*/
 
 /*
 DONE
@@ -89,7 +107,7 @@ DONE
 	Map Integration
 	Button backend + map zoom
 
-*/
+	*/
 
 
 /*
