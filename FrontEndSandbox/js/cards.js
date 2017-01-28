1322576f00c545a75cd4427415dacc7c89ec6be5
William Lee <masteratsoccer@gmail.com>
2017-01-27 22:49:20 -0500

    connected to firebase using angularfire

--- FrontEndSandbox/js/cards.js
+++ FrontEndSandbox/js/cards.js
@@ -10,11 +10,73 @@
 	access directly
 	stringify objects, keep array
 	*/
-	console.log("right outside of function" + array);
-	angular.module('MyApp')
-	.controller('AppCtrl', function($scope) {
-		console.log("right before assignment" + array);
-		$scope.eventsVar = array;
+//	console.log("right outside of function" + array);
+
+
+//angular.module('MyApp', [])
+angular.module('MyApp', ["firebase"])
+.controller('AppCtrl', function($scope, $firebaseArray) {
+	var ref = firebase.database().ref('events')/*.child("messages");*/
+  // create a synchronized array
+  // click on `index.html` above to see it used in the DOM!
+  $scope.eventsVar = $firebaseArray(ref);
+  console.log(ref);
+
+
+/*
+.controller('AppCtrl', function($scope, $firebaseArray) {
+	var ref = firebase.database().ref();
+		 // download the data into a local object
+		 $scope.data = $firebaseObject(ref);
+  // putting a console.log here won't work, see below
+  	//BECAUSE SCOPE.DATA IS LOADED ASYNCHRONOUSLY, A CONSOLE.LOG RIGHT BELOW WILL YIELD ONLY EMPTY/UNDEFINED
+
+  	*/
+
+  	/*console.log("right before assignment" + array);
+  	$scope.eventsVar = array;*/ 
+
+		 // Initialize Firebase
+
+
+	/*	 var key;
+		 var data;
+		 var array = [];
+		 var eventsArray = {};
+		 var ref = firebase.database().ref('events');
+		 var today = new Date();
+
+
+		 ref.on('value',function(snap){
+		 	snap.forEach(function(childSnapshot){
+		//console.log(JSON.stringify(childSnapshot.val()));
+		key = childSnapshot;
+		data = childSnapshot.val();
+		array.push(data); */
+
+       //console.log(data)
+       //var index = data.description.lastIndexOf(',')
+       //var data_date = data.description.substr(index-2,2)
+       //data_date = parseInt(data_date)
+       //console.log(data_date)
+       //console.log(curr_day)
+       //if(data_date < curr_day+3){ref.child("id").remove()}//removes data is from yesterday
+       //else{
+       //array.push(JSON.stringify(Event(data)));
+
+       //array.push(JSON.stringify(childSnapshot.val()));
+      // array = array.concat(JSON.stringify(childSnapshot.val()));
+       //console.log("array is " + array);
+       //console.log("concat attempt" + array);
+
+
+  /* });
+		 });
+		 eventsArray = data;
+		 console.log("events array" + eventsArray);
+
+		 */
+
 
 /*
 	//initialize eventsArray[]. Populate with array.push() from firebase
@@ -33,8 +95,6 @@
 	];*/
 
 
-
-
 	//loop through and add json elements to array
 
 	//gonna try to store json elements in a different array
@@ -46,30 +106,30 @@
     }
 });
 
-	function initMap() {
-		var uluru = {lat: 37.4419, lng: -122.1430};
+function initMap() {
+	var uluru = {lat: 37.4419, lng: -122.1430};
 
-		var map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 15,
-			center: uluru
-		});
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 15,
+		center: uluru
+	});
 
-		var marker = new google.maps.Marker({
-			position: uluru,
-			map: map
-		});
-	}
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
+}
 
-	function updateMapLocation(newLocation) {
-		var map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 15,
-			center: newLocation
-		});
+function updateMapLocation(newLocation) {
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 15,
+		center: newLocation
+	});
 
-		var marker = new google.maps.Marker({
-			position: newLocation,
-			map: map
-		});
-	}
+	var marker = new google.maps.Marker({
+		position: newLocation,
+		map: map
+	});
+}
 
 
