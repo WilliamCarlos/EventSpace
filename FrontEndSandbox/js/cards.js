5fbf59eb9dd917a185d431cff0c1582677334571
Jason Jin <jjin3@swarthmore.edu>
2017-01-30 22:48:55 -0500

    .

--- FrontEndSandbox/js/cards.js
+++ FrontEndSandbox/js/cards.js
@@ -26,17 +26,17 @@ Notes
 });
 
 	function initMap() {
-		var uluru = {lat: 37.4419, lng: -122.1430};
+		var uluru = {lat: 39.904521, lng: -75.353557};
 
 		var map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 15,
+			zoom: 17,
 			center: uluru
 		});
 
-		var marker = new google.maps.Marker({
-			position: uluru,
-			map: map
-		});
+		// var marker = new google.maps.Marker({
+		// 	position: uluru,
+		// 	map: map
+		// });
 	}
 
 	function updateMapLocation(newLocation) {
