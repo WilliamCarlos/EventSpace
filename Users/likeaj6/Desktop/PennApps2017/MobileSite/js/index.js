eff6027d32c5735548939d53d70cc587b89d1581
Jason Jin <jjin3@swarthmore.edu>
2017-02-04 19:49:48 -0500

    .

--- Users/likeaj6/Desktop/PennApps2017/MobileSite/js/index.js
+++
@@ -1,4 +0,0 @@
-angular.module('MyApp')
-  .controller('AppCtrl', function($scope) {
-    $scope.users = ['Fabio', 'Leonardo', 'Thomas', 'Gabriele', 'Fabrizio', 'John', 'Luis', 'Kate', 'Max'];
-  });
